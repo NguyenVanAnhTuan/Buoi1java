5f2a1aff7266a0add036644a92bbf2e504252105
NguyenVanAnhTuan <101329081+NguyenVanAnhTuan@users.noreply.github.com>
2022-03-10 17:57:33 +0700

    Add files via upload

---
+++ bai4.js
@@ -0,0 +1,3 @@
+document.getElementById ("demo1").innerHTML = " Tài Liệu Học CSS" 
+document.getElementById ("demo2").innerHTML = " Tài Liệu Học MySQL" ;
+document.getElementById ("demo3").innerHTML = " Tài Liệu Học PHP" ;
